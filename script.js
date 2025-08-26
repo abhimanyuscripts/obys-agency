ee743c198ec18868cbb4b6b5dd086854aafde602
Abhimanyu Tiwari <theabhimanyutiwari@gmail.com>
2025-08-26 22:27:45 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function Loadinganimation(){
+    var tl = gsap.timeline()
 
 
 tl.from(".line h1",{
@@ -43,5 +44,28 @@ tl.from("#page1",{
 })
 tl.to("#loader",{
     display:"none"
+})
+tl.from("#nav",{
+   opacity:0
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+
 }
-)
+function CursorAnimation() {
+    document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+}
+Loadinganimation()
+CursorAnimation()
